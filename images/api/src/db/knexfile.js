c72c79893147864c2ebabcc2cb7f846ee98e014e
Reem <reem.al.radi@student.ehb.be>
2023-11-17 13:25:33 +0100

    [j] - reinit

---
+++ images/api/src/db/knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+    client: 'pg',
+    connection: {
+        host: process.env.POSTGRES_HOST,
+        port: 5432,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB
+    }
+}
---
+++ images/api/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const knex = require('knex');
+// const {
+//     v4: uuid
+// } = require('uuid');
+const app = express();
+const PORT = 3000;
+const knexfile = require("./db/knexfile")
+
+// Create a Knex.js instance
+const db = knex(knexfile);
+
+db.raw("SELECT 1+1")
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.json());
+
+
+app.get('', (req, res) => {
+    res.send('gogoggo')
+});
+
+app.listen(3000, (error) => {
+    if (!error) {
+        console.log(`Server running on port ${PORT}`);
+    } else {
+        console.error(error)
+    }
+
+});
