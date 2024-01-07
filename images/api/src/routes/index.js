6d86c7c0d62bcf6af621254d17349240c4b7deba
Reem <reem.al.radi@student.ehb.be>
2024-01-07 20:21:48 +0100

    added frontend: HTML & CSS

--- images/api/src/routes/index.js
+++ images/api/src/routes/index.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const knex = require('knex');
+// const {
+//     v4: uuid
+// } = require('uuid');
 const app = express();
 const PORT = 3000;
-const knexfile = require("../db/knexfile");
+const knexfile = require("../db/knexfile")
 
+// Create a Knex.js instance
 const db = knex(knexfile);
 
+db.raw("SELECT 1+1")
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
+
+app.get('', (req, res) => {
+    res.send('gogoggo')
 });
 
-app.listen(PORT, (error) => {
+app.listen(3000, (error) => {
     if (!error) {
         console.log(`Server running on port ${PORT}`);
     } else {
-        console.error(error);
+        console.error(error)
     }
+    
+    
 });
